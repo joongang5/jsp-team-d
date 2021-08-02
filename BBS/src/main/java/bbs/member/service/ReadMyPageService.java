9be610bb878cf51f54bda48b2e67bbd91b4b1a39
WooJongSeong <whdtud@gmail.com>
2021-08-02 14:33:29 +0900

    로그인 페이지 기본 작업

---
+++ BBS/src/main/java/bbs/member/service/ReadMyPageService.java
@@ -0,0 +1,13 @@
+package bbs.member.service;
+
+import bbs.member.dao.MemberDao;
+import bbs.member.model.Member;
+
+public class ReadMyPageService {
+
+	private MemberDao memberDao = new MemberDao();
+	
+	public Member getMember() {
+		return null;
+	}
+}
