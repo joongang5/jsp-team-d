4608ad62c58d65e37a8058bfe01f8b2d66e3070f
Ho10e <84372806+Ho10e@users.noreply.github.com>
2021-08-04 22:31:34 +0900

    gg.....

--- BBS/src/main/java/bbs/notice/service/WriteRequest.java
+++ BBS/src/main/java/bbs/notice/service/WriteRequest.java
@@ -1,5 +1,36 @@
 package bbs.notice.service;
 
-public class WriteRequest {
+import java.util.Map;
 
+import bbs.notice.model.Writer;
+
+public class WriteRequest {
+	private Writer wirter;
+	private String ntitle;
+	private String ncontent;
+	
+	public WriteRequest(Writer writer, String ntitle, String ncontent) {
+		this.wirter = writer;
+		this.ntitle = ntitle;
+		this.ncontent = ncontent;
+	}
+	
+	public Writer getWirter() {
+		return wirter;
+	}
+
+	public String getNtitle() {
+		return ntitle;
+	}
+
+	public String getNcontent() {
+		return ncontent;
+	}
+	
+	public void validate(Map<String, Boolean> errors) {
+		if(ntitle == null || ntitle.trim().isEmpty()) {
+			errors.put("ntitle", Boolean.TRUE);
+		}
+	}
 }
+	
