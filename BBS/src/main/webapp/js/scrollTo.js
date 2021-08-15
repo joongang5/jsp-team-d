03f35415c4463154925b80ed597d5e0c19085910
WooJongSeong <whdtud@gmail.com>
2021-08-15 15:06:28 +0900

    포스터 페이지 이동시 5초 간격 유지되도록 수정

--- BBS/src/main/webapp/js/scrollTo.js
+++ BBS/src/main/webapp/js/scrollTo.js
@@ -1,24 +1,31 @@
-$(document).ready(function() {
+var timerId = null;
 
-	setInterval(function() {
-		var panels = document.getElementsByClassName('panel');
-		for (var i = 0; i < panels.length; i++) {
-			var panel = panels.item(i);
-			if (panel.classList.contains('selected')) {
-				panel.classList.remove('selected');
-			} else {
-				panel.classList.add('selected');
-				$('#wrapper').scrollTo(panel.getAttribute('href'), 300);		
-			}
-		}
-	}, 5000);
+$(document).ready(function() {
+	timerId = setInterval(scrollTo, 5000);
 
 	//get all link with class panel
 	$('a.panel').click(function () {
 		$('a.panel').removeClass('selected');
 		$(this).addClass('selected');
 		
-		$('#wrapper').scrollTo($(this).attr('href'), 300);		
+		$('#wrapper').scrollTo($(this).attr('href'), 300);
+		
+		clearInterval(timerId);
+		timerId = setInterval(scrollTo, 5000);
+						
 		return false;
 	});
 });
+
+function scrollTo() {
+	var panels = document.getElementsByClassName('panel');
+	for (var i = 0; i < panels.length; i++) {
+		var panel = panels.item(i);
+		if (panel.classList.contains('selected')) {
+			panel.classList.remove('selected');
+		} else {
+			panel.classList.add('selected');
+			$('#wrapper').scrollTo(panel.getAttribute('href'), 300);		
+		}
+	}
+}
